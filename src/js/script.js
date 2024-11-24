615bf62dc6a4467a74df4e8d52bffa173199a274
Tetiana <chaplinskaya.tatyana@gmail.com>
2024-11-24 22:23:53 +0100

    edd second carousel

--- src/js/script.js
+++ src/js/script.js
@@ -65,4 +65,15 @@ window.addEventListener('DOMContentLoaded', () => {
         ]
     });
 
+    $('.meet__testimonials_carousel').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.png"></button>',
+        nextArrow: '<button type="button" class= "slick-next"><img src="icons/right.png"></button>',
+        adaptiveHeight: true
+    });
+
 })
